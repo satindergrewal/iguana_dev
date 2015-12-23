99fb066ded94bd1fbc62dc0c8b0c8ec0b36e8fbc
Vineet Bhargav <vineet.bhargav86@gmail.com>
2015-12-23 12:58:08 +0530

    Defined the structure of JSON and updated the code to support it

--- js/tabs.js
+++ js/tabs.js
@@ -1,5 +1,34 @@
 var tabsManagement={};
 
+/*{
+	"panel" : {
+		"panelHeading": "Tab heading generated from template",
+		"panelBody": {
+			"tablist": [
+				{ 
+					"label" : "Peer list",
+					"active": true,
+					"content" : {
+						"table" : {
+							"id": "",
+							"headers": ["IP address", "Coin Type", "Height", "Rank", "Actions"]
+						}
+					}
+				},
+				{
+					"label" : "Favorite peers",
+					"content" : {
+						"table" : {
+							"id": "",
+							"headers": ["Peer name", "IP address", "Coin Type", "Height", "Rank", "Actions"]
+						}
+					}
+				}
+			]
+		}
+	}
+}*/
+
 //Classes
 
 tabsManagement.Tab = function(_tabData) {
@@ -11,27 +40,36 @@ tabsManagement.Tab = function(_tabData) {
 tabsManagement.loggingEnabled = true;
 tabsManagement.tabData = "{}";
 tabsManagement.templates = {
+	panel : "<div class='panel panel-default'>{{panelHeading}}{{panelBody}}</div>",
 	panelHeading : "<div class='panel-heading'><h3>{{panelHeading}}</h3></div>",
-
+	panelBody : "<div class='panel-body'>{{panelBody}}</div>",
+	tablist : "<ul class='nav nav-pills' role='tablist'>{{list.tab.label}}</ul><div class='tab-content'>{{list.tab.content}}</div>",
+	tab : { 
+		label : "<li role='presentation' class='active'><a href='#{{processedLabel}}' aria-controls='home' role='tab' data-toggle='tab'>{{label}}</a></li>",
+		content : "<div role='tabpanel' class='tab-pane active' id='{{processedLabel}}'>{{content}}</div>"
+	}
 };
 
-tabsManagement.createTab = function(element, tabData) {
-	if(tabData === null || tabData === undefined || tabData === "" || tabData === "{}") {
-		console.log("Tab Data json can not have values - null, undefined, empty string and empty json");
-		return false;
+
+
+
+tabsManagement.createTab = function(htmlresponse, parsedTabData) {
+	
+	for(var innerElement in parsedTabData) {
+		innHtmlresponse = tabsManagement.templates[innerElement];
+		if(typeof innerElement !== "string") {
+			htmlresponse = this.createTab(innHtmlresponse, innerElement);
+		} else {
+			htmlresponse = innHtmlresponse.replace("{{"+innerElement+"}}", innerElement);
+		}
 	}
 
-	var parsedTabData = JSON.parse(tabData);
-	var htmlresponse = "";
 
-	console.log("Parsed JSON Data:" + parsedTabData);
-
-	if(parsedTabData.panelHeading !== null || parsedTabData.panelHeading !== undefined) { 
+	/*if(parsedTabData.panelHeading !== null || parsedTabData.panelHeading !== undefined) { 
 		htmlresponse = tabsManagement.templates.panelHeading.replace("{{panelHeading}}", parsedTabData.panelHeading);
-	}
+	}*/
 
-	element.innerHTML = htmlresponse;
-
+	return htmlresponse;
 }
 
 tabsManagement.createTabs = function() {
@@ -41,7 +79,18 @@ tabsManagement.createTabs = function() {
 	var commonTabs = document.getElementsByClassName("common-tab");
 	for(var i=0; i < commonTabs.length; i++) {
 		var commonTab = commonTabs[i];
-		tabsManagement.createTab(commonTab, commonTab.getElementsByTagName("pre")[0].innerHTML);
+		var tabData = commonTab.getElementsByTagName("pre")[0].innerHTML;
+
+		if(tabData === null || tabData === undefined || tabData === "" || tabData === "{}") {
+			console.log("Tab Data json can not have values - null, undefined, empty string or empty json");
+			return false;
+		}
+
+		var parsedTabData = JSON.parse(tabData);
+
+		htmlresponse = tabsManagement.createTab(commonTab, parsedTabData);
+
+		commonTab.innerHTML = htmlresponse;
 	}
 }
 
